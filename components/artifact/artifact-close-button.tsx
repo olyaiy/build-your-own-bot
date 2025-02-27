88155e5e097b72682668839d88d38f4467eca68b
olyaiy <alexolyaiy@gmail.com>
2025-02-26 17:24:19 -0800

    changes

--- components/artifact/artifact-close-button.tsx
+++ components/artifact/artifact-close-button.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { CrossIcon } from '@/components/util/icons';
 import { Button } from '@/components/ui/button';
 import { initialArtifactData, useArtifact } from '@/hooks/use-artifact';
+import { UIArtifact } from './artifact';
 
 function PureArtifactCloseButton() {
   const { setArtifact } = useArtifact();
@@ -11,7 +12,7 @@ function PureArtifactCloseButton() {
       variant="outline"
       className="h-fit p-2 dark:hover:bg-zinc-700"
       onClick={() => {
-        setArtifact((currentArtifact) =>
+        setArtifact((currentArtifact: UIArtifact) =>
           currentArtifact.status === 'streaming'
             ? {
                 ...currentArtifact,
