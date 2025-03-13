5420cb91063421801326de175db5106f74080915
olyaiy <alexolyaiy@gmail.com>
2025-03-12 18:02:59 -0700

    add copy button

--- components/util/code-block.tsx
+++ components/util/code-block.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useState } from 'react';
+import { CopyButton } from '@/components/util/copy-button';
+
 interface CodeBlockProps {
   node: any;
   inline: boolean;
@@ -14,15 +17,29 @@ export function CodeBlock({
   children,
   ...props
 }: CodeBlockProps) {
+  const [isHovering, setIsHovering] = useState(false);
+
   if (!inline) {
     return (
-      <div className="not-prose flex flex-col">
+      <div 
+        className="not-prose flex flex-col relative"
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
         <pre
           {...props}
           className={`text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
         >
           <code className="whitespace-pre-wrap break-words">{children}</code>
         </pre>
+        {isHovering && (
+          <div className="absolute bottom-2 right-2">
+            <CopyButton 
+              textToCopy={children.toString()} 
+              className="bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-800 dark:hover:bg-zinc-700"
+            />
+          </div>
+        )}
       </div>
     );
   } else {
