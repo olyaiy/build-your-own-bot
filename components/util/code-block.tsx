3b7453b61ce6d49462466955b33d6d01648ec557
olyaiy <alexolyaiy@gmail.com>
2025-03-13 23:14:57 -0700

    ui

--- components/util/code-block.tsx
+++ components/util/code-block.tsx
@@ -28,7 +28,7 @@ export function CodeBlock({
       >
         <pre
           {...props}
-          className={`text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
+          className={`text-sm w-full overflow-x-auto bg-zinc-900 dark:bg-zinc-900 p-4 border border-zinc-700 dark:border-zinc-700 rounded-xl text-zinc-50 dark:text-zinc-50`}
         >
           <code className="whitespace-pre-wrap break-words">{children}</code>
         </pre>
@@ -36,7 +36,7 @@ export function CodeBlock({
           <div className="absolute bottom-2 right-2">
             <CopyButton 
               textToCopy={children.toString()} 
-              className="bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-800 dark:hover:bg-zinc-700"
+              className="bg-zinc-800 hover:bg-zinc-700 dark:bg-zinc-800 dark:hover:bg-zinc-700"
             />
           </div>
         )}
