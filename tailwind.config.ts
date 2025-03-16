a8f28c3052e3f7c09970a85b9b3eb590c3b11481
olyaiy <alexolyaiy@gmail.com>
2025-03-15 23:31:56 -0700

    fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,15 @@ const config: Config = {
   			'geist-mono'
   		]
   	},
+    screens: {
+      'xs': '480px',
+      // Tailwind's default breakpoints
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   	extend: {
   		borderRadius: {
   			lg: 'var(--radius)',
